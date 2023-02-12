b457c384ae51346237029993be61a1dc292b5f9e
+ <+>
2023-02-12 10:37:32 +0300

    completed examples

---
+++ lib/class.rb
@@ -0,0 +1,19 @@
+require "pry"
+# Here's what our Dog class would look like:
+
+class Dog
+  # some code to describe a dog
+end
+
+# Creating a new instance of a dog object using the +new+ keyword
+fido = Dog.new 
+snoopy = Dog.new
+lassie = Dog.new
+
+# CamelCased, like this:
+
+class MyClass
+  # some code all about your awesome class
+end
+
+binding.pry
